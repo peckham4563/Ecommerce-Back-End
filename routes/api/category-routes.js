83b449d16c0ca4547712dcca863137591196e9d2
unknown <peckham4563@gmail.com>
2023-02-27 18:38:38 -0600

    Edited files

---
+++ routes/api/category-routes.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", (req, res) => {
+  // find all categories
+  Category.findAll({ include: [Product] })
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => res.json(err));
+  // be sure to include its associated Products
+});
+
+router.get("/:id", (req, res) => {
+  // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => res.json(err));
+  // be sure to include its associated Products
+});
+
+router.post("/", (req, res) => {
+  // create a new category
+  Category.create(req.body)
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => res.json(err));
+});
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete("/:id", (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
